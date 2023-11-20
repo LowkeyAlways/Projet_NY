e1d54f68eecdb2f6b11a0ceedd1e28d63de46135
AndyLS <50944069+AndyL94@users.noreply.github.com>
2023-11-20 14:16:30 +0100

    Update

--- js/app_init.js
+++ js/app_init.js
@@ -22,12 +22,3 @@ export const Tamplate = {
                                     <ul>`;
     }
 };
-
-/* New York est une ville composée de 5 arrondissements à l'embouchure du fleuve Hudson et de l'océan Atlantique. En son centre se trouve Manhattan, un arrondissement densément peuplé faisant partie des principaux centres commerciaux, financiers et culturels du monde. Ses sites incontournables comprennent des gratte-ciel comme l'Empire State Building et l'immense Central Park. Le théâtre de Broadway est situé sur Times Square. ― Google
-Météo : 6°C, vent O à 6 km/h, 68 % d'humidité weather.com
-Quartiers : Harlem, Upper East Side, West Village, Flushing, SoHo, PLUS
-Population : 8,468 millions (2021)
-Altitude : 10 m; Min. −8,2 m; Max. 124,9 m
-Code FIPS : 36-51000
-Code ZIP : 100xx–104xx, 11004–05, 111xx–114xx, 116xx
-Coordonnées : 40° 42′ 51″ nord, 74° 00′ 21″ ouest */
